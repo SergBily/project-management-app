85c219284a69a5583298a75cf83f244e4f161917
SergBily <sergBily.dev@gmail.com>
2022-11-05 13:23:55 +0300

    fix: interceptor how user login

--- project-management-app/src/app/auth/interceptors/url-headers.interceptor.ts
+++ project-management-app/src/app/auth/interceptors/url-headers.interceptor.ts
@@ -8,25 +8,29 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { AuthStateService } from '../services/auth-state/auth-state.service';
 
 @Injectable()
 export class UrlHeadersInterceptor implements HttpInterceptor {
-  constructor() {}
+  headers!: HttpHeaders;
+
+  constructor(private authState: AuthStateService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const userToken: string | null = localStorage.getItem('token');
-    const headers = new HttpHeaders({
+
+    this.headers = new HttpHeaders({
       'Content-Type': 'application/json',
       accept: 'application/json',
     });
 
-    if (userToken) {
-      headers.set('Authorization', `Bearer ${userToken}`);
+    if (this.authState.getCurrentState()) {
+      this.headers = this.headers.set('Authorization', `Bearer ${userToken}`);
     }
 
     const newRequest = request.clone({
       url: environment.baseUrl + request.url,
-      headers,
+      headers: this.headers,
     });
     return next.handle(newRequest);
   }
