28df1a59425454b8fdd5a55f7039f482f393d166
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2022-11-11 18:36:08 +0300

    refactor: change styles boards list

--- project-management-app/src/app/main/main.module.ts
+++ project-management-app/src/app/main/main.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { BoardsListComponent } from './components/boards-list/boards-list.component';
 import { BoardItemComponent } from './components/board-item/board-item.component';
 import { AddBoardComponent } from './components/add-board/add-board.component';
+
+const MATERIAL = [MatCardModule, MatButtonModule, MatIconModule];
 
 @NgModule({
   declarations: [
@@ -18,6 +23,7 @@ import { AddBoardComponent } from './components/add-board/add-board.component';
     CommonModule,
     MainRoutingModule,
     ReactiveFormsModule,
+    MATERIAL,
   ],
 })
 export class MainModule { }
